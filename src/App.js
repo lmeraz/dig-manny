91d869ef5363427d425806ebfc73ab19a84ab76f
Matt Ipavec <matt@Matts-MacBook-Pro.local>
2018-05-05 13:18:45 -0700

    api called

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+import Map from './components/map/map.js';
+import Times from './components/times/times.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to EDC</h1>
+        </header>
+        <div className="row">
+          <div className="col-sm-8"> 
+            <Map/>
+          </div>
+          <div className="col-sm-4">
+            <Times/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
