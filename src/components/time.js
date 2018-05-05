4e6e1d516bee461f69588e4bb5e9ce5f96fe95f1
Umpoy <iumpoy@yahoo.com>
2018-05-05 12:25:25 -0700

    made components folder

---
+++ src/components/time.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <h1>This will be waiting time area </h1>
+    )
+}
---
+++ src/time.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+export default Time {
+    return (<h1>Hello</h1>)
+}
