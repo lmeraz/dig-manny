039002d6b2e9de765fee1e7a42b2e1746a47f323
Umpoy <iumpoy@yahoo.com>
2018-05-05 17:06:55 -0700

    buttons show color depending on data

--- src/components/times/times.js
+++ src/components/times/times.js
@@ -15,37 +15,38 @@ class Times extends React.Component {
     getTimes = () => {
         axios.get('https://pt04hn1ypb.execute-api.us-west-2.amazonaws.com/prod')
 
-        .then(response =>{
-            let data = response.data.times;
-            console.log(data);
-            debugger;
-            const listItems = data.map((waitTime, station)=>{
-                return <tr key= {waitTime}><td>{station+1}</td><td>{waitTime}</td></tr>
-            }
-            
-        )
-        // switch(waitTime) {
-        //     case (waitTime >= 15):
-        //         break;
-        //     case (waitTime < 15 && waitTime > 5):
-        //         console.log("test");
-        //         break;
-        //     case (waitTime <= 5 && waitTime > 0):
-        //         break;
-        //     default:
-        // }
-        
-        this.setState({
-                time: listItems
+            .then(response => {
+                let data = response.data.times;
+                console.log(data);
+                debugger;
+                const listItems = data.map((waitTime, station) => {
+                    let waitTimeColor = waitTime >= 15 ? 'btn btn-danger' : waitTime > 6 ? 'btn btn-warning' : 'btn btn-success'
+                    return <tr key={waitTime}><td>{station + 1}</td><td><button type="button" className={waitTimeColor}></button></td></tr>
+                }
+
+                )
+                // switch(waitTime) {
+                //     case (waitTime >= 15):
+                //         break;
+                //     case (waitTime < 15 && waitTime > 5):
+                //         console.log("test");
+                //         break;
+                //     case (waitTime <= 5 && waitTime > 0):
+                //         break;
+                //     default:
+                // }
+
+                this.setState({
+                    time: listItems
+                })
+
+                // this.setState({
+                //     time1: data[0],
+                //     time2: data[1],
+                //     time3: data[2],
+                //     time4: data[3]
+                // })
             })
-        
-            // this.setState({
-            //     time1: data[0],
-            //     time2: data[1],
-            //     time3: data[2],
-            //     time4: data[3]
-            // })
-        })
 
     }
 
