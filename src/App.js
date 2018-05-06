ab5ec1348f924323f58a6a9af022aaa3c20c76c2
Matt Ipavec <matt@Matts-MacBook-Pro.local>
2018-05-05 18:57:37 -0700

    bubbles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Map from './components/map/map.js';
 import Times from './components/times/times.js';
 import Key from './components/key/key.js';
+import bubbles from './images/bubbles.gif';
 
 class App extends Component {
   render() {
@@ -11,7 +12,9 @@ class App extends Component {
         {/* <header className="App-header">
           <h1 className="App-title">Welcome to EDC</h1>
         </header> */}
+        
         <div className="row">
+          {/* <img class= "bubbles" src = {bubbles} alt= "bubbles"/> */}
           <div className= "col-sm-1"/> 
           <div className="col-sm-6"> 
             <Map/>
